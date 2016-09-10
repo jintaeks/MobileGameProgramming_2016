920b458dfb1516c4e45a3a73b3f7d713e62ea514
jintaeks <jintaeks@gmail.com>
2016-09-10 12:28:38 +0900

    commit test...

--- ConsoleApplication/Algorithm/Algorithm.cpp
+++ ConsoleApplication/Algorithm/Algorithm.cpp
@@ -6,6 +6,7 @@ void main()
     int iNumber = 0;
     std::string strResult;
 
+    // added comment. jintaeks on 2016-09-09_12-40
     std::cin >> iNumber;
     std::cout << "number is " << iNumber << std::endl;
 }//main()
